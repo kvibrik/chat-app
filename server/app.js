80b01d91e1b8a997947361500ee21e09f4ab27d7
kvibrik <kvibr@mail.ru>
2021-01-20 15:57:10 +0500

    подключил сокет

--- server/app.js
+++ server/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+io.on('connection', socket => {
+  console.log('new user connected');
+});
 
 const PORT = 3000;
 
