748fb11e505738a70844ae5f70ee77b130c8f5ac
kvibrik <kvibr@mail.ru>
2021-01-20 15:53:09 +0500

    удалил все лишнее

--- server/app.js
+++ server/app.js
@@ -1,16 +1,9 @@
 const express = require('express');
-const users = require('./routes/users');
-
 const app = express();
+const http = require('http').createServer(app);
+
 const PORT = 3000;
-const ROUTES = {
-  users: '/users',
-};
 
-app.use(express.json());
-// Routes
-app.use(ROUTES.users, users);
-
-app.listen(PORT, () => {
+http.listen(PORT, () => {
   console.log(`Server up and running, PORT: ${PORT}`);
 });
