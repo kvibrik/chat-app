2ed82e9abf47289e9d7de6e630b094c714e19ad0
kvibrik <kvibr@mail.ru>
2021-01-25 22:06:16 +0500

    временный конфиг для firebase

---
+++ chat/src/plugins/firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBL-L_MrU0ej703a_AnQ7t4fIqUP15FhOw",
+  authDomain: "chat-app-30246.firebaseapp.com",
+  projectId: "chat-app-30246",
+  storageBucket: "chat-app-30246.appspot.com",
+  messagingSenderId: "571576911474",
+  appId: "1:571576911474:web:51ecaa9e1c9e4ffb0ee4eb"
+};
+
+export default firebaseConfig;
