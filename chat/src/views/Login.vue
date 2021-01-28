07a0d6264a69a635c2047fe150072a250ceccadf
kvibrik <kvibr@mail.ru>
2021-01-28 17:36:33 +0500

    верстка страницы с логином

--- chat/src/views/Login.vue
+++ chat/src/views/Login.vue
@@ -1,7 +1,47 @@
 <template>
-  <div>Login</div>
+  <div class="login-page">
+    <div class="login-form-wrap">
+      <div class="login-form-image">
+        <img src="@/assets/login-page-bg.png" alt="login-page-bg" />
+      </div>
+      <!-- LoginForm -->
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: 'Login',
+};
 </script>
+
+<style scoped>
+.login-page {
+  background-color: #d5e4e4;
+  height: 100vh;
+  min-height: 780px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.login-form-wrap {
+  width: 100%;
+  max-width: 1000px;
+  background-color: #fff;
+  border-radius: 20px;
+  display: flex;
+  align-items: center;
+  box-shadow: 0 30px 90px #a7d7d9;
+  height: 600px;
+  overflow: hidden;
+}
+.login-form-image {
+  height: 100%;
+  overflow: hidden;
+}
+.login-form-image img {
+  display: block;
+  height: 100%;
+  transform: scale(1.2);
+}
+</style>
