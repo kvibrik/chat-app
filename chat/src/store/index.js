e670d7e888c5253f8f4b39dacf767b953d769382
kvibrik <kvibr@mail.ru>
2021-02-02 22:34:27 +0500

    отслеживание изменений логина в сторе

--- chat/src/store/index.js
+++ chat/src/store/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import firebase from '@/plugins/firebase';
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  },
+const store = new Vuex.Store({
+  state: {},
+  mutations: {},
+  actions: {},
+  modules: {},
 });
+
+firebase.auth().onAuthStateChanged((user) => {
+  console.log('onAuthStateChange', user);
+});
+
+export default store;
