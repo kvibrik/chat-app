7b7af434bd6bbce8d08df81c06bc74c42d2d8b47
kvibrik <kvibr@mail.ru>
2021-02-06 18:35:25 +0500

    стора для аутентификации

---
+++ chat/src/store/modules/auth.js
@@ -0,0 +1,15 @@
+const authStore = {
+  namespaced: true,
+  state: {
+    isLoggedIn: false,
+    loginInProgress: false,
+  },
+  getters: {
+    isLoggedIn: ({ isLoggedIn }) => isLoggedIn,
+    loginInProgress: ({ loginInProgress }) => loginInProgress,
+  },
+  actions: {},
+  mutations: {},
+};
+
+export default authStore;
