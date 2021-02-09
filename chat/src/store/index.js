139e9cfc9e3e96f225e65eb56f17d4e3bdcfbaa3
kvibrik <kvibr@mail.ru>
2021-02-09 17:53:42 +0500

    добавление юзера в стору

--- chat/src/store/index.js
+++ chat/src/store/index.js
@@ -1,10 +1,11 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import firebase from '@/plugins/firebase';
-// import authGuard from '@/guards/auth.guard';
+import authGuard from '@/guards/auth.guard';
 import router from '@/router';
 import auth from './modules/auth';
 import notify from './modules/notify';
+import user from './modules/user';
 
 Vue.use(Vuex);
 
@@ -15,18 +16,20 @@ const store = new Vuex.Store({
   modules: {
     auth,
     notify,
+    user,
   },
 });
 
-firebase.auth().onAuthStateChanged((user) => {
-  console.log('onAuthStateChange', user);
-  store.dispatch('setIsLoggedInState', Boolean(user));
+firebase.auth().onAuthStateChanged((userData) => {
+  console.log('onAuthStateChange', userData);
+  store.dispatch('setIsLoggedInState', Boolean(userData));
+  store.dispatch('setUserState', userData);
 
-  if (user) {
+  if (userData) {
     router.push({ name: 'Home' });
   }
 });
 
-// authGuard();
+authGuard(store);
 
 export default store;
