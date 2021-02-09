898b216d838e01f44b4a1724718eecc1651c2b9f
kvibrik <kvibr@mail.ru>
2021-02-09 15:15:29 +0500

    подключение сторы нотификаций в общую стору

--- chat/src/store/index.js
+++ chat/src/store/index.js
@@ -4,6 +4,7 @@ import firebase from '@/plugins/firebase';
 // import authGuard from '@/guards/auth.guard';
 import router from '@/router';
 import auth from './modules/auth';
+import notify from './modules/notify';
 
 Vue.use(Vuex);
 
@@ -13,6 +14,7 @@ const store = new Vuex.Store({
   actions: {},
   modules: {
     auth,
+    notify,
   },
 });
 
